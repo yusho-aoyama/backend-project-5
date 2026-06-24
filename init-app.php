635128dfcfb73c0f64ae6ada73656895e4f47309
Yusho-VM <yusho-ubuntu@vm.com>
2026-06-24 03:09:43 +0000

    mysqli: Implement database connection

--- init-app.php
+++ init-app.php
@@ -5,7 +5,48 @@ spl_autoload_register(function($class) {
     if (file_exists(stream_resolve_include_path($file))) include($file);
 });
 
-use Helpers\Settings;
 
-printf("Local database username: %s\n", Settings::env('DATABASE_USER'));
-printf("Local database password (hashed): %s\n", password_hash(Settings::env('DATABASE_USER_PASSWORD'), PASSWORD_DEFAULT));
+use Database\MySQLWrapper;
+
+$mysqli = new MySQLWrapper();
+
+printf("Connected database: %s\n", $mysqli->getDatabaseName());
+
+use Helpers\Settings;
+/*
+    接続の失敗時にエラーを報告し、例外をスローします。データベース接続を初期化する前にこの設定を行ってください。
+    テストするには、.env設定で誤った情報を入力します。
+*/
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+/*
+ * https://www.php.net/manual/en/class.mysqli.php で利用可能なすべてのメソッドを確認できます。
+ */
+$mysqli = new mysqli(
+    'localhost', 
+    Settings::env('DATABASE_USER'), 
+    Settings::env('DATABASE_USER_PASSWORD'), 
+    Settings::env('DATABASE_NAME')
+);
+
+// https://www.php.net/manual/en/mysqli.get-charset.php
+$charset = $mysqli->get_charset();
+
+if($charset === null) throw new Exception('Charset could be read');
+
+// データベースの文字セット、照合順序、統計情報について取得します。
+printf(
+    "%s's charset: %s.%s",
+    Settings::env('DATABASE_NAME'),
+    $charset->charset,
+    PHP_EOL
+);
+
+printf(
+    "collation: %s.%s",
+    $charset->collation,
+    PHP_EOL
+);
+
+// 接続を閉じるには、closeメソッドを使用します。
+$mysqli->close();
