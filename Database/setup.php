a36eb57d5831d8ce7eb0fe7684d540a67f7a98e3
Yusho-VM <yusho-ubuntu@vm.com>
2026-06-26 15:27:58 +0000

    Update setup.php for SQL-based migration

--- Database/setup.php
+++ Database/setup.php
@@ -9,24 +9,12 @@ $mysqli = new MySQLWrapper();
 | Create cars table
 |--------------------------------------------------------------------------
 */
-$result = $mysqli->query("
-    CREATE TABLE IF NOT EXISTS cars (
-      id INT PRIMARY KEY AUTO_INCREMENT,
-      make VARCHAR(50),
-      model VARCHAR(50),
-      year INT,
-      color VARCHAR(20),
-      price FLOAT,
-      mileage FLOAT,
-      transmission VARCHAR(20),
-      engine VARCHAR(20),
-      status VARCHAR(10)
-    );
-");
 
-if ($result === false) {
-    throw new Exception('Could not create cars table.');
-}
+$result = $mysqli->query(
+    file_get_contents(__DIR__ . '/Examples/cars-setup.sql')
+);
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -35,18 +23,15 @@ if ($result === false) {
 */
 $result = $mysqli->query("
     CREATE TABLE IF NOT EXISTS car_parts (
-        id INT PRIMARY KEY AUTO_INCRFEMENT,
+        id INT PRIMARY KEY AUTO_INCREMENT,
         carID INT NOT NULL,
         name VARCHAR(255),
         description TEXT,
         price FLOAT,
-        quantityInStock INT.
+        quantityInStock INT,
         FOREIGN KEY (carID) REFERENCES cars(id) 
     );
 ");
 
-if ($result === false) {
-    throw new Exception ('Could not create car_parts table');
-}
-
-print("Successfully ran all SQL setup queries." . PHP_EOL);
+if($result === false) throw new Exception('Could not execute query.');
+else print("Successfully ran all SQL setup queries.".PHP_EOL);
