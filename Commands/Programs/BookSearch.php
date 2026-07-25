adc3e32a88505a0e2f3dad7a0c914b0f29351c1e
Yusho-VM <yusho-ubuntu@vm.com>
2026-07-25 03:18:31 +0000

    add base CLI for book search

---
+++ Commands/Programs/BookSearch.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Commands\Programs;
+
+use Commands\AbstractCommand;
+use Commands\Argument;
+
+class BookSearch extends AbstractCommand
+{
+    // Aliases
+    protected static ?string $alias = 'book-search';
+
+    
+    /**
+     * このコマンドで使用できるオプションを定義する
+     *
+     * --isbn  : ISBNで検索
+     * --title : タイトルで検索
+     */
+    public static function getArguments(): array
+    {
+         return [
+            // isbn
+            (new Argument('isbn'))
+                ->description('Search for a book by ISBN.')
+                ->required(false)
+                ->allowAsShort(true),
+
+            // title
+            (new Argument('title'))
+                ->description('Search for books by title.')
+                ->required(false)
+                ->allowAsShort(true),
+        ];
+    }
+
+
+    public function execute(): int
+    {
+        $isbn = $this->getArgumentValue('isbn');
+        $title = $this->getArgumentValue('title');
+
+        // isbnとtitleが両方指定された場合はエラー
+        if ($isbn !== false && $title !== false) {
+            $this->log('Please use either --isbn or --title, not both.');
+
+            return 1;
+        }
+
+        // isbnとtitleのどちらも指定されていない場合はエラー
+        if ($isbn === false && $title === false) {
+            $this->log('Please provide --isbn or --title.');
+
+            return 1;
+        }
+
+        // isbnが指定されている場合
+        if ($isbn !== false) {
+            $this->log("Searching by ISBN: {$isbn}");
+
+            return 0;
+        }
+
+
+        // ここまで来た場合はtitleが指定されている
+        $this->log("Searching by title: {$title}");
+
+        return 0;
+            
+        }
+}
--- Commands/registry.php
+++ Commands/registry.php
@@ -7,4 +7,5 @@ return [
     Commands\Programs\Migrate::class,
     Commands\Programs\CodeGeneration::class,
     Commands\Programs\DbWipe::class,
+    Commands\Programs\BookSearch::class,
 ];
