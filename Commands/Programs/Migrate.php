6c96329eff8cba92339498acbd2da06b443d9021
Yusho-VM <yusho-ubuntu@vm.com>
2026-08-01 23:44:49 +0000

    Add init option and migrations table setup

--- Commands/Programs/Migrate.php
+++ Commands/Programs/Migrate.php
@@ -14,28 +14,55 @@ class Migrate extends AbstractCommand
     public static function getArguments(): array
     {
         return [
-            (new Argument('rollback'))->description('Roll backwards. An integer n may also be provided to rollback n times.')->required(false)->allowAsShort(true),
+            (new Argument('rollback'))
+                ->description('Roll backwards. An integer n may also be provided to rollback n times.')
+                ->required(false)
+                ->allowAsShort(true),
+
+            (new Argument('init'))
+                ->description("Create the migrations table if it doesn't exist.")
+                ->required(false)
+                ->allowAsShort(true),
         ];
     }
 
     public function execute(): int
     {
         $rollback = $this->getArgumentValue('rollback');
-        if($rollback === false){
+
+        if ($this->getArgumentValue('init')) $this->createMigrationsTable();
+
+        if ($rollback === false) {
             $this->log("Starting migration......");
             $this->migrate();
-        }
-        else{
-            // ロールバックは設定されている場合はtrue、またはそれに添付されている値が整数として表されます。
-            $rollback = $rollback === true ? 1 : (int) $rollback;
+        } else {
+            // rollbackはtrueに設定されているか、それに関連付けられた値が整数として存在するかのいずれかです。
+            $rollbackN = $rollback === true ? 1 : (int)$rollback;
             $this->log("Running rollback....");
-            for($i = 0; $i < $rollback; $i++){
-                $this->rollback();
-            }
+            $this->rollback($rollbackN);
         }
 
         return 0;
     }
+
+    private function createMigrationsTable(): void
+    {
+        $this->log("Creating migrations table if necessary...");
+
+        $mysqli = new MySQLWrapper();
+
+        $result = $mysqli->query("
+            CREATE TABLE IF NOT EXISTS migrations (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                filename VARCHAR(255) NOT NULL
+            );
+        ");
+
+        if ($result === false) throw new \Exception("Failed to create migration table.");
+
+        $this->log("Done setting up migration tables.");
+    }
+
     private function migrate(): void {
         $this->log("Running migrations...");
         $this->log("Migration ended...\n");
