3de6b7cb391ecc3f54f7056440912bb3e3b4d53a
Yusho-VM <yusho-ubuntu@vm.com>
2026-08-01 05:33:00 +0000

    add initial user and post migrations

---
+++ Database/Migrations/2026-08-01_1785558458_CreateUserTable.php
@@ -0,0 +1,31 @@
+<?php
+namespace Database\Migrations;
+
+use Database\SchemaMigration;
+
+class CreateUserTable implements SchemaMigration
+{
+    public function up(): array
+    {
+        // マイグレーションロジック
+         return [
+            "CREATE TABLE users (
+                id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                username VARCHAR(255) NOT NULL,
+                email VARCHAR(255) NOT NULL UNIQUE,
+                password VARCHAR(255) NOT NULL,
+                email_confirmed_at VARCHAR(255),
+                created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+            )"
+        ];
+    }
+
+    public function down(): array
+    {
+        // ロールバックロジック
+        return [
+            "DROP TABLE users"
+        ];
+    }
+}
